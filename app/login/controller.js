f9baf33d8050f8ab92b079a75ec3206f015fecb1
Kamil Sobik <kamil.sobik@gmail.com>
2022-05-14 11:31:37 +0200

    logginAs git commit -m logginAs git commit -m logginAs

--- app/login/controller.js
+++ app/login/controller.js
@@ -2,12 +2,13 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { inject as service } from '@ember/service';
+import { storageFor } from 'ember-local-storage';
 
 export default class LoginController extends Controller {
   @service store;
-  @tracked
-  loginValue;
+  @tracked loginValue;
   @tracked passwordValue;
+  @storageFor('logged-as') loggedAs;
 
   @action
   onLoginChange(event) {
@@ -22,10 +23,14 @@ export default class LoginController extends Controller {
   @action
   async onSubmit(event) {
     event.preventDefault();
-    console.log('onSubmit- triggered');
     const users = await this.store.query('user', {
       filter: { username: this.loginValue, password: this.passwordValue },
     });
-    console.log(users.length);
+
+    const isUserExist = Boolean(users.length);
+    if (isUserExist) {
+      const user = users.firstObject;
+      this.loggedAs.set('id', user.id);
+    }
   }
 }
