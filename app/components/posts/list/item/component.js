e4266c54b2e5794fba57153ff302a0ac5208a6b0
Kamil Sobik <kamil.sobik@gmail.com>
2022-06-04 10:48:35 +0200

    add like and dislike

--- app/components/posts/list/item/component.js
+++ app/components/posts/list/item/component.js
@@ -1,14 +1,41 @@
 import Component from '@glimmer/component';
 import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
 
 export default class PostsListItemComponent extends Component {
   @service session;
+  @service store;
+
+  get currentUser() {
+    return this.session.currentUser
+  }
+
+get currentPost() {
+  return this.args.post
+}
+
+  @action 
+  async onLike() {
+    console.log('like');
+    const createdLike = this.store.createRecord('like', {
+      user: this.currentUser, 
+      post: this.currentPost
+    })
+    await createdLike.save();
+    }
+
+  @action 
+  async onDislike() {
+    const like = this.currentUser.likes.find((like) => {
+      return like.post.get('id') === this.currentPost.id;
+    })
+ await like.destroyRecord() 
+  }
 
   get isAlreadyLikedByCurrenUser() {
-    const { currentUser } = this.session;
     const { post } = this.args;
-    const likeModel = currentUser.likes.find((like) => {
-      return like.post.get('id') === post.id;
+    const likeModel = this.currentUser.likes.find((like) => {
+      return like.post.get('id') === this.currentPost.id;
     });
     const isLikeExists = Boolean(likeModel);
     return isLikeExists;
