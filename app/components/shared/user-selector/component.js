6af9ec16a49113bfce4ac1971f5351bcf4a113b9
Kamil Sobik <kamil.sobik@gmail.com>
2022-07-01 21:49:17 +0200

    create user selector component

---
+++ app/components/shared/user-selector/component.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+
+export default class SharedUserSelectorComponent extends Component {
+  @service store;
+
+  constructor() {
+    super(...arguments);
+
+    this.options = this.args.options || this.store.findAll('user');
+  }
+}
--- app/home/posts/controller.js
+++ app/home/posts/controller.js
@@ -2,20 +2,12 @@ import Controller from '@ember/controller';
 import moment from 'moment';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
 
 export default class HomePostsController extends Controller {
-  @service store;
   @tracked dateFrom;
   @tracked dateTo;
   @tracked sort;
   @tracked selectedAuthors = [];
-
-  constructor() {
-    super(...arguments);
-
-    this.authors = this.store.findAll('user');
-  }
 
   queryParams = ['dateFrom', 'dateTo', 'sort'];
 
@@ -138,6 +130,6 @@ export default class HomePostsController extends Controller {
 
   @action
   chooseAuthors(author) {
-    this.selectedAuthors = author;
+    this.selectedAuthors = Array.isArray(author) ? author : [author];
   }
 }
