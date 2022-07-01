3770a577ba5663499b91289b0ceae19b0e5d15e7
Kamil Sobik <kamil.sobik@gmail.com>
2022-07-01 22:03:32 +0200

    fix lint tests

--- app/home/posts/route.js
+++ app/home/posts/route.js
@@ -4,8 +4,8 @@ import { inject as service } from '@ember/service';
 export default class PostsRoute extends Route {
   @service store;
 
-  async model() {
-    const posts = await this.store.findAll('post');
+  model() {
+    const posts = this.store.findAll('post');
     return posts;
   }
 }
