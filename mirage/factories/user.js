62c9b32b29b4c7ee69adb09fdf8ae5e4c6232950
Kamil Sobik <kamil.sobik@gmail.com>
2022-07-02 16:23:53 +0200

    fix mirage factoru user

--- mirage/factories/user.js
+++ mirage/factories/user.js
@@ -2,8 +2,16 @@ import { Factory } from 'miragejs';
 import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
-  username: faker.name.firstName(),
-  password: faker.internet.password(20),
-  email: faker.internet.email(),
-  photoURL: faker.image.avatar(),
+  username() {
+    return faker.name.firstName();
+  },
+  password() {
+    return faker.internet.password(20);
+  },
+  email() {
+    return faker.internet.email();
+  },
+  photoURL() {
+    return faker.image.avatar();
+  },
 });
