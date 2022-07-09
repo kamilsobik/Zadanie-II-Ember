51576429abb53cefd5401dcbfa900b8b4e11cfab
Kamil Sobik <kamil.sobik@gmail.com>
2022-07-09 14:00:27 +0200

    add loader for user route

--- app/home/users/route.js
+++ app/home/users/route.js
@@ -5,6 +5,14 @@ export default class UsersRoute extends Route {
   @service store;
 
   async model() {
+    console.log('start');
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        console.log('dupa');
+        resolve();
+      }, 2000);
+    });
+
     const users = await this.store.findAll('user');
     return users;
   }
